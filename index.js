f690f6ce003045f28c8c0470e87fbeb7e77f4f44
LakshmiMarneedi <nagavaralakshmimarneedi5@gmail.com>
2023-07-11 06:32:29 +0000

    SuperOerLeague

---
+++ index.js
@@ -0,0 +1,11 @@
+const element = (
+  //   Write your code here.
+  <div className="background">
+    <h1 className="heading">Super Over League</h1>
+    <div className="d-flex">
+        <img className="image" src ="https://assets.ccbp.in/frontend/react-js/rcb-img.png"/>
+        <img className="image" src="https://assets.ccbp.in/frontend/react-js/csk-img.png"/>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
